56dd13509e13cf843fe5a12723d38f9a8c193f69
Arun Jose <12997751+ArunJose@users.noreply.github.com>
2020-01-28 15:48:30 +0530

    Updates

---
+++ precache-manifest.82f7742f8241d6f61aed81c41ce931bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102fa3b333ba6702babe9b473df2dac5",
+    "url": "/bookfinder/index.html"
+  },
+  {
+    "revision": "8d019a3862ae4886a032",
+    "url": "/bookfinder/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9900930bcdc0ff5618a8",
+    "url": "/bookfinder/static/js/2.3ca009c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bookfinder/static/js/2.3ca009c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d019a3862ae4886a032",
+    "url": "/bookfinder/static/js/main.e5f0fe1f.chunk.js"
+  },
+  {
+    "revision": "bbadce772d5c57db8290",
+    "url": "/bookfinder/static/js/runtime-main.6eb9dbf4.js"
+  }
+]);
