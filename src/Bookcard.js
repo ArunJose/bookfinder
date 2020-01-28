5723c38f089528f49b0b878d92188d7141aa2b5b
Arun Jose <12997751+ArunJose@users.noreply.github.com>
2020-01-28 19:05:59 +0530

    Card style changed

--- src/Bookcard.js
+++ src/Bookcard.js
@@ -5,18 +5,22 @@ const Bookcard = props => {
   const authors = props.book.authors ? props.book.authors : null;
   return (
     <div className="Bookcard">
-      {props.book.imageLinks && (
-        <img src={props.book.imageLinks.smallThumbnail} alt="Book Cover" />
-      )}
-      <h4>{props.book.title}</h4>
-      <i>{authors && `By ${authors.join(", ")}`}</i>
-      <br />
-      {props.book.publisher && `Published by ${props.book.publisher}`}
-      <br />
-      <a href={props.book.infoLink} target="_blank" rel="noopener noreferrer">
-        <button>More</button>
-      </a>
-      <br />
+      <div className="container">
+        {props.book.imageLinks && (
+          <img src={props.book.imageLinks.smallThumbnail} alt="Book Cover" />
+        )}
+        <h4>{props.book.title}</h4>
+        <i>{authors && `By ${authors.join(", ")}`}</i>
+        <br />
+        <small>
+          {props.book.publisher && `Published by ${props.book.publisher}`}
+        </small>
+        <br />
+        <a href={props.book.infoLink} target="_blank" rel="noopener noreferrer">
+          <button>More</button>
+        </a>
+        <br />
+      </div>
     </div>
   );
 };
